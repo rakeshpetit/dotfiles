a91a6d762be0af051cd6d550ebfcec9cfb0e045a
Rakesh Arunachalam <rakesh@thoughtbot.com>
2023-06-30 15:35:29 +0100

    Delete all files

--- zsh/configs/history.zsh
+++
@@ -1,6 +0,0 @@
-setopt hist_ignore_all_dups inc_append_history hist_ignore_space share_history
-HISTFILE=~/.zhistory
-HISTSIZE=10000
-SAVEHIST=10000
-
-export ERL_AFLAGS="-kernel shell_history enabled"
